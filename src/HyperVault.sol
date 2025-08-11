c7ee0e4cec3c09788a4a5eaaef6a9ca12bdb278f
Adam O'Callaghan <adam.ocallaghan@yahoo.ie>
2025-08-11 14:55:43 +0100

    importing ERC4626, CoreWriterLib + PercompileLib, basic contract done

--- src/HyperVault.sol
+++ src/HyperVault.sol
@@ -6,99 +6,33 @@ import {CoreWriterLib, HLConversions} from "@hyper-evm-lib/src/CoreWriterLib.sol
 import {PrecompileLib} from "@hyper-evm-lib/src/PrecompileLib.sol";
 
 /**
- * @title VaultExample
- * @dev This contract demonstrates CoreWriterLib vault functionality.
+ * @title HyperVault
+ * @dev ERC4626 vault to tokenise vaults/staking on HyperCore
  */
-contract VaultExample is ERC4626 {
+contract HyperVault is ERC4626 {
     using CoreWriterLib for *;
 
     uint64 public constant USDC_TOKEN_ID = 0;
 
     /*//////////////////////////////////////////////////////////////
-                        Basic Vault Operations
+                    Hooks for Basic Vault Operations
     //////////////////////////////////////////////////////////////*/
 
-    /**
-     * @notice Deposits USDC to a specified vault
-     * @param vault Address of the vault to deposit to
-     * @param usdcAmount Amount of USDC to deposit
-     */
-    function depositToVault(address vault, uint64 usdcAmount) external {
-        CoreWriterLib.vaultTransfer(vault, true, usdcAmount);
-    }
-
-    /**
-     * @notice Withdraws USDC from a specified vault
-     * @param vault Address of the vault to withdraw from
-     * @param usdcAmount Amount of USDC to withdraw
-     */
-    function withdrawFromVault(address vault, uint64 usdcAmount) external {
+    function beforeWithdraw(uint256 assets, uint256 shares) internal virtual {
         CoreWriterLib.vaultTransfer(vault, false, usdcAmount);
     }
 
-    /*//////////////////////////////////////////////////////////////
-                        Advanced Vault Operations
-    //////////////////////////////////////////////////////////////*/
-
-    /**
-     * @notice Withdraws USDC from vault and sends to recipient
-     * @dev vaultTransfer checks if funds are withdrawable and reverts if locked
-     * @param vault Address of the vault to withdraw from
-     * @param recipient Address to send the withdrawn USDC to
-     * @param coreAmount Amount of USDC to withdraw and send
-     */
-    function withdrawFromVaultAndSend(address vault, address recipient, uint64 coreAmount) external {
-        uint64 usdcPerpAmount = HLConversions.convertUSDC_CoreToPerp(coreAmount);
-
-        CoreWriterLib.vaultTransfer(vault, false, usdcPerpAmount);
-
-        CoreWriterLib.transferUsdClass(usdcPerpAmount, false);
-
-        CoreWriterLib.spotSend(recipient, USDC_TOKEN_ID, coreAmount);
-    }
-
-    /**
-     * @notice Transfers USDC from spot to perp and deposits to vault
-     * @param vault Address of the vault to deposit USDC to
-     * @param coreAmount Amount of USDC to transfer and deposit to vault
-     */
-    function transferUsdcToPerpAndDepositToVault(address vault, uint64 coreAmount) external {
-        uint64 usdcPerpAmount = HLConversions.convertUSDC_CoreToPerp(coreAmount);
-
-        CoreWriterLib.transferUsdClass(usdcPerpAmount, true);
-
-        CoreWriterLib.vaultTransfer(vault, true, usdcPerpAmount);
+    function afterDeposit(uint256 assets, uint256 shares) internal virtual {
+        CoreWriterLib.vaultTransfer(vault, true, usdcAmount);
     }
 
     /*//////////////////////////////////////////////////////////////
                         Vault Information
     //////////////////////////////////////////////////////////////*/
 
-    /**
-     * @notice Gets the vault equity for a user in a specific vault
-     * @param user Address of the user to check
-     * @param vault Address of the vault to check
-     * @return equity Amount of equity the user has in the vault
-     * @return lockedUntilTimestamp Timestamp until which the equity is locked
-     */
-    function getVaultEquity(address user, address vault)
-        public
-        view
-        returns (uint64 equity, uint64 lockedUntilTimestamp)
-    {
-        PrecompileLib.UserVaultEquity memory vaultEquity = PrecompileLib.userVaultEquity(user, vault);
-        return (vaultEquity.equity, vaultEquity.lockedUntilTimestamp);
-    }
-
-    /**
-     * @notice Checks if funds are withdrawable from a vault for a user
-     * @param user Address of the user to check
-     * @param vault Address of the vault to check
-     * @return withdrawable True if funds can be withdrawn, false if locked
-     */
-    function isWithdrawable(address user, address vault) public view returns (bool withdrawable) {
-        PrecompileLib.UserVaultEquity memory vaultEquity = PrecompileLib.userVaultEquity(user, vault);
-        return block.timestamp >= vaultEquity.lockedUntilTimestamp;
+    function totalAssets() public view override returns (uint256) {
+        PrecompileLib.UserVaultEquity memory vaultEquity = PrecompileLib.userVaultEquity(address(this), vault);
+        return vaultEquity.equity;
     }
 
     receive() external payable {}
